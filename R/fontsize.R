0c3270e74f49ca4212fc64988d998de470bfd3f1
Artur Quirino <arturhgq@mail.com>
2022-10-18 18:49:49 -0300

    `geom_text_fontsize()` translation

--- R/fontsize.R
+++ R/fontsize.R
@@ -1,12 +1,13 @@
-#' @title Adequa a escala de pontos do ggplot2 à escala de texto
-#' @description
-#' @param .size
+#' @title Match ggplot2 pt scale to text scale
+#' @description `r lifecycle::badge("stable")`
+#' This function matchs ggplot2 pt scale to text scale
+#' @param size text scale size
 #' @references
 #' https://stackoverflow.com/questions/52304141/adjust-font-size-to-size-of-plot-device
 #' https://stackoverflow.com/questions/17311917/ggplot2-the-unit-of-size
 #' https://www.tidyverse.org/blog/2020/08/taking-control-of-plot-scaling/
 #' https://www.christophenicault.com/post/understand_size_dimension_ggplot2/
 #' @export
-geom_text_fontsize <- \(.size = 12) {
-    .size/ggplot2::.pt * .8
+geom_text_fontsize <- \(size = 12) {
+  size/ggplot2::.pt * .8
 }
