2b8e2ed0840ec263598ed46f3800a9845f50f674
Artur Quirino <arturhgq@mail.com>
2022-11-05 23:27:05 -0300

    `geom_col_wrap`

---
+++ R/geom_col.R
@@ -0,0 +1,70 @@
+#' @title plot col wrap
+#' @examples
+#' ggplot2::presidential |>
+#' dplyr::mutate(
+#'   days = as.numeric(end - start) + 1
+#' ) -> data
+#'
+#' plot_col_wrap(
+#'   data,
+#'   mapping = ggplot2::aes(x = name, y = days),
+#'   width = .8,
+#'   group = c(party),
+#'   size = .1,
+#'   fill = "gray"
+#' )
+#' @export
+plot_col_wrap = function(
+    data = NULL,
+    mapping = NULL,
+    stat = "identity",
+    position = "dodge",
+    key_glyph = NULL,
+    width = NULL,
+    na.rm = FALSE,
+    show.legend = NA,
+    inherit.aes = TRUE,
+    group,
+    lab_y,
+    ...) {
+
+  group_args = as.list(
+    do.call("substitute", alist(group))[-1]
+  )
+  group_data = do.call(
+    dtools::split_group,
+    c(list(data), group_args)
+  )
+  params = c(
+    width = width,
+    na.rm = na.rm,
+    list(...)
+  )
+
+  purrr::imap(
+    .x = group_data,
+    ~ {
+      call_args = list(
+        ggplot2 = ggplot2::ggplot(),
+        layer =  ggplot2::layer(
+          mapping = mapping,
+          data = .x,
+          stat = stat,
+          geom = ggplot2::GeomCol,
+          position = position,
+          show.legend = show.legend,
+          inherit.aes = inherit.aes,
+          key_glyph = key_glyph,
+          params = params
+        ),
+        labs = ggplot2::labs(title = .y)
+      )
+      purrr::reduce(call_args, `+`)
+    }
+  )
+}
+
+
+
+
+
--- R/theme_basic.R
+++ R/theme_basic.R
@@ -41,104 +41,105 @@ theme_basic = function(
 #    axis_title_x = TRUE,
 #    axis_title_y = TRUE
 ) {
-  ggplot2::theme(
-    text = ggplot2::element_text(
-      size = size,
-      color = color_text,
-      lineheight = 1,
-      family = family
-    ),
-    plot.margin = ggplot2::margin(
-      t = margin_plot_t,
-      r = margin_plot_r,
-      b = margin_plot_b,
-      l = margin_plot_l
-    ),
-    plot.title.position = "plot",
-    plot.caption.position = "plot",
-    plot.title = ggtext::element_textbox_simple(
-      size = size_title,
-      face = "bold",
-      color = color("black"),
-      margin = ggplot2::margin(0,0,2,0)
-    ),
-    plot.subtitle = ggtext::element_textbox_simple(
-      size = size_subtitle,
-      color = color("textgray"),
-      margin = ggplot2::margin(0,0,2,0)
-    ),
-    plot.caption = ggtext::element_textbox_simple(
-      size = size_caption,
-      color = color("black"),
-      halign = 1,
-      lineheight = 1,
-      margin = ggplot2::margin(2,0,0,0)
-    ),
-    strip.background = ggplot2::element_rect(
-      color = color_strip_background,
-      fill = fill_strip_background
-    ),
-    strip.text = ggtext::element_textbox_simple(
-      size = size_strip,
-      margin = ggplot2::margin(
-        t = margin_strip_t,
-        r = margin_strip_r,
-        b = margin_strip_b,
-        l = margin_strip_l
+  ggplot2::theme_grey() +
+    ggplot2::theme(
+      text = ggplot2::element_text(
+        size = size,
+        color = color_text,
+        lineheight = 1,
+        family = family
       ),
-      padding = ggplot2::margin(4, 4, 2, 4),
-      halign = halign_strip_text,
-      face = face_strip_text
-    ),
-    panel.background = ggplot2::element_blank(),
-    panel.spacing = ggplot2::unit(panel_spacing, "lines"),
-    axis.ticks.y = ggplot2::element_blank(),
-    axis.ticks.length.x = ggplot2::unit(length_ticks, "cm"),
-    axis.ticks.x = ggplot2::element_line(size = .2, color = color_ticks_x),
-    axis.line.y = ggplot2::element_blank(),
-    axis.line.x = ggplot2::element_line(size = .2, color = color_line_x),
-    axis.title.y = ggtext::element_markdown(
-      angle = angle_axis_title_y,
-      hjust = vjust_axis_title_y,
-      color = color_axis_title,
-      size = size_axis_title,
-      margin = ggplot2::margin(r = 5)
-    ),
-    axis.title.x = ggtext::element_markdown(
-      margin = ggplot2::margin(t = 5, b = margin_axis_title_x_b),
-      color = color_axis_title,
-      size = size_axis_title,
-      hjust = hjust_axis_title_x
-    ),
-    axis.text.y = ggtext::element_markdown(
-      size = size_axis,
-      color = color_text,
-      vjust = 0
-    ),
-    axis.text.x = ggtext::element_markdown(
-      size = size_axis,
-      color = color_text
-    ),
-    panel.grid = ggplot2::element_blank(),
-    panel.grid.major.y = ggplot2::element_line(
-      color = color("linegray"),
-      size = size_grid_major_line
-    ),
-    legend.position = pos_legend,
-    legend.background = ggplot2::element_blank(),
-    legend.key = ggplot2::element_blank(),
-    legend.key.size = ggplot2::unit(1, 'lines'),
-    legend.text = ggplot2::element_text(
-      size = size_legend,
-      family = family,
-      color = color("text")
-    ),
-    legend.title = ggplot2::element_blank(),
-    legend.margin = ggplot2::margin(
-      t = margin_legend_t,
-      r = margin_legend_r,
-      b = margin_legend_b,
-      l = margin_legend_l
+      plot.margin = ggplot2::margin(
+        t = margin_plot_t,
+        r = margin_plot_r,
+        b = margin_plot_b,
+        l = margin_plot_l
+      ),
+      plot.title.position = "plot",
+      plot.caption.position = "plot",
+      plot.title = ggtext::element_textbox_simple(
+        size = size_title,
+        face = "bold",
+        color = color("black"),
+        margin = ggplot2::margin(0,0,2,0)
+      ),
+      plot.subtitle = ggtext::element_textbox_simple(
+        size = size_subtitle,
+        color = color("textgray"),
+        margin = ggplot2::margin(0,0,2,0)
+      ),
+      plot.caption = ggtext::element_textbox_simple(
+        size = size_caption,
+        color = color("black"),
+        halign = 1,
+        lineheight = 1,
+        margin = ggplot2::margin(2,0,0,0)
+      ),
+      strip.background = ggplot2::element_rect(
+        color = color_strip_background,
+        fill = fill_strip_background
+      ),
+      strip.text = ggtext::element_textbox_simple(
+        size = size_strip,
+        margin = ggplot2::margin(
+          t = margin_strip_t,
+          r = margin_strip_r,
+          b = margin_strip_b,
+          l = margin_strip_l
+        ),
+        padding = ggplot2::margin(4, 4, 2, 4),
+        halign = halign_strip_text,
+        face = face_strip_text
+      ),
+      panel.background = ggplot2::element_blank(),
+      panel.spacing = ggplot2::unit(panel_spacing, "lines"),
+      axis.ticks.y = ggplot2::element_blank(),
+      axis.ticks.length.x = ggplot2::unit(length_ticks, "cm"),
+      axis.ticks.x = ggplot2::element_line(size = .2, color = color_ticks_x),
+      axis.line.y = ggplot2::element_blank(),
+      axis.line.x = ggplot2::element_line(size = .2, color = color_line_x),
+      axis.title.y = ggtext::element_markdown(
+        angle = angle_axis_title_y,
+        hjust = vjust_axis_title_y,
+        color = color_axis_title,
+        size = size_axis_title,
+        margin = ggplot2::margin(r = 5)
+      ),
+      axis.title.x = ggtext::element_markdown(
+        margin = ggplot2::margin(t = 5, b = margin_axis_title_x_b),
+        color = color_axis_title,
+        size = size_axis_title,
+        hjust = hjust_axis_title_x
+      ),
+      axis.text.y = ggtext::element_markdown(
+        size = size_axis,
+        color = color_text,
+        vjust = 0
+      ),
+      axis.text.x = ggtext::element_markdown(
+        size = size_axis,
+        color = color_text
+      ),
+      panel.grid = ggplot2::element_blank(),
+      panel.grid.major.y = ggplot2::element_line(
+        color = color("linegray"),
+        size = size_grid_major_line
+      ),
+      legend.position = pos_legend,
+      legend.background = ggplot2::element_blank(),
+      legend.key = ggplot2::element_blank(),
+      legend.key.size = ggplot2::unit(1, 'lines'),
+      legend.text = ggplot2::element_text(
+        size = size_legend,
+        family = family,
+        color = color("text")
+      ),
+      legend.title = ggplot2::element_blank(),
+      legend.margin = ggplot2::margin(
+        t = margin_legend_t,
+        r = margin_legend_r,
+        b = margin_legend_b,
+        l = margin_legend_l
+      )
     )
-  )
 }
